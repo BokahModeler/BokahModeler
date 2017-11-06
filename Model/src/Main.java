8c7e969198ed3d93b6b16b9024fbc1d6f6a2bde0
KylerSchmidt <KylerKSchmidt@gmail.com>
2017-11-06 14:55:51 -0500

    made image loader loop. few patches

--- Model/src/Main.java
+++ Model/src/Main.java
@@ -57,7 +57,7 @@ public class Main {
             loop();
             glfwDestroyWindow(window);
         } finally {
-        	System.exit(0);
+        	f.setVisible(true);
             glfwTerminate();
         }
     }
@@ -119,9 +119,10 @@ public class Main {
     	
     	glRotatef(1,(float)y,(float)x,0f);
     	 
-    	glBindTexture(GL_TEXTURE_2D, getTexture(img));//must go before glbegin. will move inside loop once we have an array of textures.
-        glBegin(GL_QUADS);
+    	
         for(ArrayList<float[]> polygon: nose){//nose should be calculated in another class//will probably neeed a for loop to get texture
+        	glBegin(GL_QUADS);
+        	glBindTexture(GL_TEXTURE_2D, getTexture(img));//must go before glbegin. will move inside loop once we have an array of textures.
         	float[] topLeft = polygon.get(0);
         	float[] topRight = polygon.get(1);
         	float[] bottomRight = polygon.get(2);
@@ -134,8 +135,9 @@ public class Main {
         	glVertex3f(bottomRight[0], bottomRight[1], bottomRight[2]);
         	glTexCoord2f(0,1);
         	glVertex3f(bottomLeft[0], bottomLeft[1], bottomLeft[2]);
+        	glEnd();
         }
-        glEnd();
+        
     	
     }
    public int getTexture(BufferedImage image){//have a texture class
@@ -232,4 +234,6 @@ public class Main {
 		f.pack();
 		f.setVisible(true);
     }
+    
 }
+
